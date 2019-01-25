e510b6f90125277389f7e11a565885b0191b18d8
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-01-25 16:36:38 +0100

    2 new exception classes.

---
+++ framework/src/main/java/com/ta/framework/exceptions/BrowserException.java
@@ -0,0 +1,34 @@
+package com.ta.framework.exceptions;
+/**
+ * 
+ * @author gentjan kolicaj
+ *
+ */
+public class BrowserException extends RuntimeException{	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3379800134953635127L;
+
+	public BrowserException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BrowserException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BrowserException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BrowserException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ framework/src/main/java/com/ta/framework/exceptions/SupplierException.java
@@ -0,0 +1,36 @@
+package com.ta.framework.exceptions;
+/**
+ * 
+ * @author gentjan koliçaj
+ *
+ */
+public class SupplierException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5591104040114428031L;
+
+	public SupplierException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SupplierException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SupplierException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SupplierException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
