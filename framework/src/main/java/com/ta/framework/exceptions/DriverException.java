ee7d73c347bfb47f60918908c9b39977d20024db
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-01-28 12:09:47 +0100

    2 new exception classes.

---
+++ framework/src/main/java/com/ta/framework/exceptions/DriverException.java
@@ -0,0 +1,29 @@
+package com.ta.framework.exceptions;
+/**
+ * 
+ * @author gentjan kolicaj
+ *
+ */
+public class DriverException extends RuntimeException{
+
+	public DriverException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DriverException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DriverException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DriverException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ framework/src/main/java/com/ta/framework/exceptions/TestDataException.java
@@ -0,0 +1,36 @@
+package com.ta.framework.exceptions;
+/**
+ * 
+ * @author gentjan kolicaj
+ *
+ */
+public class TestDataException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7677782576124892099L;
+
+	public TestDataException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestDataException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestDataException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestDataException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
